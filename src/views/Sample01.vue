e860165054550bf2d9e83adbd2f9191e6651b8ab
yuexushuang <yuexushuang@163.com>
2021-01-07 10:05:02 +0800

    完成obj模型加载
    完成obj模型加载

--- src/views/Sample01.vue
+++ src/views/Sample01.vue
@@ -6,8 +6,9 @@
 </template>
 <script>
     import * as Three from 'three'
-    import { MTLLoader, OBJLoader } from 'three-obj-mtl-loader'
-    let OrbitControls = require('three-orbit-controls')(Three)
+    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+    import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+    import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
     export default {
         name: "Sample01",
@@ -27,26 +28,36 @@
             init: function () {
                 let container = this.$el;
 
+                //初始化相机和场景
                 this.camera =
                     new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 1, 2000)
-                this.camera.position.z = 250
-
                 this.scene = new Three.Scene()
+                //给场景添加一个坐标轴，长度是100，红X，绿Y，蓝Z
+                let axesHelper = new Three.AxesHelper(10);
+                //axesHelper.position.set(0, 0, -10);
+                this.scene.add(axesHelper);
 
-                const ambientLight = new Three.AmbientLight(0xcccccc, 0.4);
+                //给场景添加环境光，给相机添加点光源
+                const ambientLight = new Three.AmbientLight(0xcccccc, 1);
                 this.scene.add(ambientLight);
-                const pointLight = new Three.PointLight(0xffffff, 0.8);
+                const pointLight = new Three.PointLight(0xffffff, 1);
                 this.camera.add(pointLight);
 
-                let controls = new OrbitControls(this.camera)
-
+                //加载obj模型
                 this.loadObj();
 
+                //设置相机位置，看向原点
+                this.camera.position.z = 3
+                this.camera.position.y = 3
+                this.camera.position.x = 3
+
+                //初始化render，并添加到dom中
                 this.renderer = new Three.WebGLRenderer({ antialias: true })
                 this.renderer.setSize(container.clientWidth, container.clientHeight)
                 container.appendChild(this.renderer.domElement)
 
-
+                //添加控制器
+                let controls = new OrbitControls(this.camera, this.renderer.domElement)
             },
             animate: function () {
                 requestAnimationFrame(this.animate)
@@ -55,7 +66,7 @@
 
             loadObj() {
                 let mtlLoader = new MTLLoader();
-                mtlLoader.load('./models/test.mtl', function (materials) {
+                mtlLoader.load('./models/test.mtl', (materials) => {
 
                     const loader = new OBJLoader();
                     loader.setMaterials(materials);
@@ -63,21 +74,18 @@
                         './models/test.obj',
                         (object) => {
                             this.scene.add(object);
-                            object.scale.set(10, 10, 10);
-                            object.position.y = - 95;
+                            //object.scale.set(10, 10, 10);
+                            //object.position.y = - 95;
                         },
                         function (xhr) {
                             console.log((xhr.loaded / xhr.total * 100) + '% loaded');
                         },
                         function (error) {
+                            throw error;
                             console.log('An error happened');
                         }
                     );
                 });
-
-
-
-
             }
         }
     }
